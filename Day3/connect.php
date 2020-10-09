2456ec1a175dac99b3926d6e0e80f5260bd2a6e3
Suriansh <71491986+Suriansh@users.noreply.github.com>
2020-10-09 22:53:35 +0530

    Revert "Day3"
    This reverts commit 4e4f99d00f0f53f3f5bce5eaa5907bef2cacab33.

--- Day3/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$connect= mysqli_connect("localhost","root","","result")or die(mysqli_error());
-
-echo"connected!";
-
-?>
--- Day3/q2.php
+++
@@ -1,7 +0,0 @@
-<?php
-require("connect.php");
-$update = mysqli_query( $connect ,"UPDATE class1 SET sub5='99' where name='Rohan'");
-
-
-
-?>
